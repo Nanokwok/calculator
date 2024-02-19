cc1ae98262b78365116541e5f0a7c833beb15d9c
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-02-19 21:11:08 +0700

    add docstring

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 class Model:
+    """Model class for storing calculation history."""
+
     def __init__(self):
+        """Initialize the Model."""
         self.history = []
 
     def save(self, expression, result):
+        """Save the expression and result to the history."""
         self.history.append((expression, result))
-
-
